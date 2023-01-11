6660b3e9c0419560719c707b4a0521416e78b2cf
Rinbo <robin.b@outlook.com>
2023-01-11 21:49:18 +0100

    Add deps and config

---
+++ src/main/java/nu/borjessons/airhockeyserver/AirHockeyServerApplication.java
@@ -0,0 +1,11 @@
+package nu.borjessons.airhockeyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirHockeyServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AirHockeyServerApplication.class, args);
+	}
+}
