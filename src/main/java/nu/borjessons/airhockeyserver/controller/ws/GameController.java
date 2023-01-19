583f82b4a6954d5e83fc667a793ba5d8c9664875
Rinbo <robin.b@outlook.com>
2023-01-19 22:06:57 +0100

    Resove issue with non-existant httpservletreq

--- src/main/java/nu/borjessons/airhockeyserver/controller/ws/GameController.java
+++ src/main/java/nu/borjessons/airhockeyserver/controller/ws/GameController.java
@@ -1,15 +1,18 @@
 package nu.borjessons.airhockeyserver.controller.ws;
 
+import java.util.Map;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
-import jakarta.servlet.http.HttpSession;
 import nu.borjessons.airhockeyserver.model.Agent;
 import nu.borjessons.airhockeyserver.model.ChatMessage;
 import nu.borjessons.airhockeyserver.model.LobbyId;
@@ -19,6 +22,19 @@ import nu.borjessons.airhockeyserver.service.api.GameService;
 @Controller
 public class GameController {
   private static final Logger logger = LoggerFactory.getLogger(GameController.class);
+
+  private static String getAttribute(SimpMessageHeaderAccessor header, String key) {
+    return getMap(header).map(map -> (String) map.get(key)).orElse("");
+  }
+
+  private static Optional<Map<String, Object>> getMap(SimpMessageHeaderAccessor header) {
+    return Optional.ofNullable(header.getSessionAttributes());
+  }
+
+  private static void setAttribute(SimpMessageHeaderAccessor header, String key, String value) {
+    getMap(header).ifPresent(map -> map.put(key, value));
+  }
+
   private final GameService gameService;
   private final SimpMessagingTemplate messagingTemplate;
 
@@ -27,21 +43,23 @@ public class GameController {
     this.gameService = gameService;
   }
 
-  @MessageMapping("/chat/{roomId}")
-  public void handleChat(@DestinationVariable String roomId, @Payload ChatMessage chatMessage) {
-    messagingTemplate.convertAndSend("/topic/room-" + roomId, chatMessage);
+  @MessageMapping("/lobby/{lobbyId}")
+  public void handleChat(@DestinationVariable String lobbyId, @Payload ChatMessage chatMessage, SimpMessageHeaderAccessor header) {
+    logger.info("{} in lobby-{} sent a message", getAttribute(header, "username"), getAttribute(header, "lobbyId"));
+
+    messagingTemplate.convertAndSend("/topic/lobby-" + lobbyId, chatMessage);
   }
 
-  @MessageMapping("/chat/connect/{lobbyId}")
-  public void handleConnect(@DestinationVariable String lobbyId, @Payload ChatMessage chatMessage, HttpSession session) {
+  @MessageMapping("/lobby/{lobbyId}/connect")
+  public void handleConnect(@DestinationVariable String lobbyId, @Payload ChatMessage chatMessage, SimpMessageHeaderAccessor header) {
     Username username = chatMessage.username();
     gameService.addUserToLobby(LobbyId.ofString(lobbyId), username);
 
-    session.setAttribute("username", username.toString());
-    session.setAttribute("roomId", lobbyId);
+    setAttribute(header, "username", username.toString());
+    setAttribute(header, "lobbyId", lobbyId);
 
-    messagingTemplate.convertAndSend("/topic/room-" + lobbyId,
-        new ChatMessage(new Username(Agent.GAME_ADMIN.toString()), username + "joined", chatMessage.datetime()));
+    messagingTemplate.convertAndSend("/topic/lobby-" + lobbyId,
+        new ChatMessage(new Username(Agent.GAME_ADMIN.toString()), username + " joined", chatMessage.datetime()));
   }
 
   @MessageMapping("/send-message")
