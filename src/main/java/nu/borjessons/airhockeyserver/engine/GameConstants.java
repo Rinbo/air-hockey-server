cffc9ffab367f7fcfa5f9d08a4165a02065b4a09
Rinbo <robin.b@outlook.com>
2023-02-13 20:03:14 +0100

    Add logic for updating puck and handle collisions

--- src/main/java/nu/borjessons/airhockeyserver/engine/GameConstants.java
+++ src/main/java/nu/borjessons/airhockeyserver/engine/GameConstants.java
@@ -5,11 +5,12 @@ package nu.borjessons.airhockeyserver.engine;
  */
 final class GameConstants {
   static final double BOARD_ASPECT_RATIO = 1.6; // height / width
-  static final int FRAME_RATE = 60;
+  static final int FRAME_RATE = 30;
   static final double GOAL_WIDTH = 0.2;
   static final double HANDLE_RADIUS = 0.1;
   static final Position PUCK_START_P2 = new Position(0.5, 0.4);
   static final double PUCK_W_RADIUS = 0.08;
+  static final double PUCK_HANDLE_MIN_DISTANCE = PUCK_W_RADIUS + HANDLE_RADIUS;
   static final double PUCK_H_RADIUS = PUCK_W_RADIUS * BOARD_ASPECT_RATIO;
   static final Speed ZERO_SPEED = new Speed(0, 0);
   private static final Position HANDLE_START_P1 = new Position(0.5, 0.8);
--- src/main/java/nu/borjessons/airhockeyserver/engine/GameRunnable.java
+++ src/main/java/nu/borjessons/airhockeyserver/engine/GameRunnable.java
@@ -3,6 +3,8 @@ package nu.borjessons.airhockeyserver.engine;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,8 +29,29 @@ class GameRunnable implements Runnable {
     this.gameId = gameId;
   }
 
+  // TODO implement max speed
+  private static GameObject bounceOffHandle(GameObject puck) {
+    return new GameObject(puck.position(), reverseSpeed(puck.speed()));
+  }
+
+  private static double calculatePuckHandleDistance(Position puckPosition, Position handlePosition) {
+    return Math.sqrt(Math.pow(puckPosition.x() - handlePosition.x(), 2) + Math.pow(puckPosition.y() - handlePosition.y(), 2));
+  }
+
   private static BroadcastState createBroadcastState(Position opponentPosition, Position puckPosition) {
     return new BroadcastState(opponentPosition, puckPosition);
+  }
+
+  private static GameObject impartHandleSpeedOnPuck(GameObject puck, GameObject player) {
+    return new GameObject(puck.position(), player.speed());
+  }
+
+  private static boolean isSpeedZero(Speed speed) {
+    return speed.x() == 0 && speed.y() == 0;
+  }
+
+  private static Speed reverseSpeed(Speed speed) {
+    return new Speed(speed.x() * -1, speed.y() * -1);
   }
 
   @Override
@@ -40,6 +63,7 @@ class GameRunnable implements Runnable {
 
     while (!Thread.currentThread().isInterrupted()) {
       try {
+        updatePuckPositionAndSpeed();
         Collision collision = detectCollision();
         handleCollision(collision);
         broadcast(playerOneTopic, playerTwoTopic);
@@ -78,6 +102,12 @@ class GameRunnable implements Runnable {
     if (puckPosition.y() - GameConstants.PUCK_H_RADIUS <= 0)
       return Collision.TOP_WALL;
 
+    if (calculatePuckHandleDistance(puckPosition, boardState.playerOne().position()) <= GameConstants.PUCK_HANDLE_MIN_DISTANCE)
+      return Collision.P1_HANDLE;
+
+    if (calculatePuckHandleDistance(puckPosition, boardState.playerTwo().position()) <= GameConstants.PUCK_HANDLE_MIN_DISTANCE)
+      return Collision.P2_HANDLE;
+
     return Collision.NO_COLLISION;
   }
 
@@ -85,31 +115,65 @@ class GameRunnable implements Runnable {
     switch (collision) {
       case LEFT_WALL, RIGHT_WALL -> reversePuckXDirection();
       case TOP_WALL, BOTTOM_WALL -> reversePuckYDirection();
-      case P1_HANDLE, P2_HANDLE, NO_COLLISION, default -> logger.warn("unknown collision type: {}", collision);
+      case P1_HANDLE -> onP1HandleCollision();
+      case P2_HANDLE -> onP2HandleCollision();
+      case NO_COLLISION, default -> logger.warn("unknown collision type: {}", collision);
     }
   }
 
+  private void onP1HandleCollision() {
+    onPlayerHandleCollision(BoardState::playerOne);
+  }
+
+  private void onP2HandleCollision() {
+    onPlayerHandleCollision(BoardState::playerTwo);
+  }
+
+  private void onPlayerHandleCollision(Function<BoardState, GameObject> playerSelector) {
+    atomicReference.getAndUpdate(boardState -> {
+      GameObject player = playerSelector.apply(boardState);
+      GameObject puck = boardState.puck();
+
+      GameObject newPuckState = isSpeedZero(player.speed()) ? impartHandleSpeedOnPuck(puck, player) : bounceOffHandle(puck);
+
+      return new BoardState(
+          newPuckState,
+          boardState.playerOne(),
+          boardState.playerTwo());
+    });
+  }
+
   private void reversePuckXDirection() {
+    updatePuckSpeed((speed, speed2) -> new Speed(-1 * speed.x(), speed.y()));
+  }
+
+  private void reversePuckYDirection() {
+    updatePuckSpeed((speed, speed2) -> new Speed(speed.x(), -1 * speed.y()));
+  }
+
+  private void updatePuckPositionAndSpeed() {
     atomicReference.getAndUpdate(boardState -> {
       GameObject puck = boardState.puck();
+      Position position = puck.position();
+      Speed speed = puck.speed();
 
       return new BoardState(
           new GameObject(
-              puck.position(),
+              new Position(position.x() + speed.x(), position.y() + speed.y()),
               new Speed(-1 * puck.speed().x(), puck.speed().y())),
           boardState.playerOne(),
           boardState.playerTwo());
     });
   }
 
-  private void reversePuckYDirection() {
+  private void updatePuckSpeed(BinaryOperator<Speed> speedUpdater) {
     atomicReference.getAndUpdate(boardState -> {
       GameObject puck = boardState.puck();
+      Speed speed = puck.speed();
+      Speed updatedSpeed = speedUpdater.apply(speed, speed);
 
       return new BoardState(
-          new GameObject(
-              puck.position(),
-              new Speed(puck.speed().x(), -1 * puck.speed().y())),
+          new GameObject(puck.position(), updatedSpeed),
           boardState.playerOne(),
           boardState.playerTwo());
     });
