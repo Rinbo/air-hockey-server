8a4944335559ffe6b97022c449007ecc5083887a
Robin Börjesson <robin.b@outlook.com>
2023-04-02 13:11:20 +0200

    Change to static snapshot version, add prod props

--- src/main/java/nu/borjessons/airhockeyserver/AirHockeyServerApplication.java
+++ src/main/java/nu/borjessons/airhockeyserver/AirHockeyServerApplication.java
@@ -1,23 +1,11 @@
 package nu.borjessons.airhockeyserver;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AirHockeyServerApplication {
-  private static final Logger logger = LoggerFactory.getLogger(AirHockeyServerApplication.class);
-
   public static void main(String[] args) {
     SpringApplication.run(AirHockeyServerApplication.class, args);
   }
-
-/*  @Bean
-  CommandLineRunner checkSslConfiguration(ServletWebServerApplicationContext applicationContext) {
-    return args -> {
-      SslStoreProvider sslStoreProvider = applicationContext.getBean(SslStoreProvider.class);
-      logger.info("Keystore loaded successfully with aliases: {}", sslStoreProvider.getKeyStore().aliases());
-    };
-  }*/
 }
