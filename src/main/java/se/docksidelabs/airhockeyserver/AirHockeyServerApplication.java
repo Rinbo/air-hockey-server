dfb5f5c27965267aef03857d642f57abde16abc7
Robin Börjesson <robin.b@outlook.com>
2026-03-08 15:46:18 +0100

    adapt for deployment to fly.io

--- src/main/java/se/docksidelabs/airhockeyserver/AirHockeyServerApplication.java
+++ src/main/java/se/docksidelabs/airhockeyserver/AirHockeyServerApplication.java
@@ -1,11 +1,17 @@
 package se.docksidelabs.airhockeyserver;
 
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import se.docksidelabs.airhockeyserver.model.GameId;
+import se.docksidelabs.airhockeyserver.model.Player;
+import se.docksidelabs.airhockeyserver.model.Username;
+
 @SpringBootApplication
 @EnableScheduling
+@RegisterReflectionForBinding({ Player.class, Username.class, GameId.class })
 public class AirHockeyServerApplication {
   public static void main(String[] args) {
     SpringApplication.run(AirHockeyServerApplication.class, args);
